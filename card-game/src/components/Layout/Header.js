d5340044d3ad09c52d5694eed87e462c94946379
KonstantinaStrantzali <konstrantz@gmail.com>
2022-10-18 10:00:16 +0000

    fix cart-button

--- card-game/src/components/Layout/Header.js
+++ card-game/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {Fragment} from 'react'
 import classes from './Header.module.css'
 import coffeeImage from '../../assets/coffee.avif';
+import HeaderButton from './HeaderButton';
 
 function Header() {
   return (
     <Fragment>
         <header className={classes.header}>
             <h1>Instant Coffee</h1>
-            <button>Cart</button>
+            <HeaderButton>Cart</HeaderButton>
         </header>
         <div className={classes['main-image']}>{/* Since there is a dash name we cant use dot notation */}
         <img src={coffeeImage} alt="Delicious coffee"></img>
