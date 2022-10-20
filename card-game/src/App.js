8d117163420cd1bbfab77296b1e7fc7840f67c01
KonstantinaStrantzali <konstrantz@gmail.com>
2022-10-20 07:44:48 +0000

    portal applied in ReactDom

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import Drinks from './components/Drinks/Drinks'
+import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header';
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Drinks/>
--- card-game/src/components/Cart/Cart.js
+++ card-game/src/components/Cart/Cart.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
 
 function Cart() {
  const cartItems = <ul className={classes['cart-items']}>{[{ id : 'c1', name : "Sushi", amount: "2", price: 12.00}].map((item) => 
  <li>{item.name}</li>)} </ul>
   return (
-    <div>
+    <Modal>
     <div>{cartItems}</div>
     <div className= {classes.total}></div>
-    <div className = {classes.actions}></div>
+    <div className = {classes.actions}>
     <button className={classes['button--alt']}>Close</button>
     <button className={classes.button}>Order</button>
     </div>
+    </Modal>
   )
 }
 
